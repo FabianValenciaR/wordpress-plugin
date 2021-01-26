4d7c46592307f1fa194c026795f0336de243f269
Fabian Valencia <fabianmvr@gmail.com>
2021-01-26 15:32:23 -0500

    Creating first hooks with menu and submenu

--- plugin-name.php
+++ plugin-name.php
@@ -8,17 +8,17 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              https://contentrefinery.io/
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           Content_Refinery
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       Content Refinery
+ * Plugin URI:        https://contentrefinery.io/
+ * Description:       At The Content Refinery, we visualize intent data to refine content topics, enhance nurture campaigns and measure attribution.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Content Refinery
+ * Author URI:        https://contentrefinery.io/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-name
